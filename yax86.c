72c2be904a663e653e80cf3aa854d71b74d9099e
Chuan Ji <chuan@jichu4n.com>
2025-06-23 23:51:28 -0700

    Add group 2 instructions; tests TODO.

--- yax86.c
+++ yax86.c
@@ -186,8 +186,7 @@ static OperandValue ReadRegisterWord(
 }
 
 // Table of Read* functions, indexed by OperandAddressType and Width.
-static OperandValue (*const kReadOperandValueFn[kNumOperandAddressTypes]
-                                               [kNumWidths])(
+static OperandValue (*const kReadOperandValueFn[kNumOperandAddressTypes][kNumWidths])(
     CPUState* cpu, const OperandAddress* address) = {
     // kOperandTypeRegister
     {ReadRegisterByte, ReadRegisterWord},
@@ -268,6 +267,12 @@ static const int32_t kMaxSignedValue[kNumWidths] = {
 static const int32_t kMinSignedValue[kNumWidths] = {
     -0x80,   // kByte
     -0x8000  // kWord
+};
+
+// Number of bits in each data width.
+static const uint8_t kNumBits[kNumWidths] = {
+    8,   // kByte
+    16,  // kWord
 };
 
 // Add an 8-bit signed relative offset to a 16-bit unsigned base address.
@@ -1700,6 +1705,220 @@ static ExecuteInstructionStatus ExecuteGroup1InstructionWithSignExtension(
 }
 
 // ============================================================================
+// Group 2 - ROL, ROR, RCL, RCR, SHL, SHR, SAL, SAR
+// ============================================================================
+
+typedef ExecuteInstructionStatus (*Group2ExecuteInstructionFn)(
+    const InstructionContext* ctx, Operand* op, uint8_t count);
+
+// SHL r/m8, 1
+// SHL r/m16, 1
+// SHL r/m8, CL
+// SHL r/m16, CL
+static ExecuteInstructionStatus ExecuteGroup2Shl(
+    const InstructionContext* ctx, Operand* op, uint8_t count) {
+  // Return early if count is 0, so as to not affect flags.
+  if (count == 0) {
+    return kExecuteSuccess;
+  }
+  uint32_t value = FromOperand(op);
+  uint32_t result = (value << count) & kMaxValue[ctx->metadata->width];
+  WriteOperand(ctx, op, result);
+  bool last_msb =
+      ((value << (count - 1)) & kSignBit[ctx->metadata->width]) != 0;
+  SetFlag(ctx->cpu, kCF, last_msb);
+  if (count == 1) {
+    bool current_msb = ((result & kSignBit[ctx->metadata->width]) != 0);
+    SetFlag(ctx->cpu, kOF, last_msb != current_msb);
+  }
+  SetCommonFlagsAfterInstruction(ctx, result);
+  return kExecuteSuccess;
+}
+
+// SHR r/m8, 1
+// SHR r/m16, 1
+// SHR r/m8, CL
+// SHR r/m16, CL
+static ExecuteInstructionStatus ExecuteGroup2Shr(
+    const InstructionContext* ctx, Operand* op, uint8_t count) {
+  // Return early if count is 0, so as to not affect flags.
+  if (count == 0) {
+    return kExecuteSuccess;
+  }
+  uint32_t value = FromOperand(op);
+  uint32_t result = value >> count;
+  WriteOperand(ctx, op, result);
+  bool last_lsb = ((value >> (count - 1)) & 1) != 0;
+  SetFlag(ctx->cpu, kCF, last_lsb);
+  if (count == 1) {
+    bool original_msb = ((value & kSignBit[ctx->metadata->width]) != 0);
+    SetFlag(ctx->cpu, kOF, original_msb);
+  }
+  SetCommonFlagsAfterInstruction(ctx, result);
+  return kExecuteSuccess;
+}
+
+// SAR r/m8, 1
+// SAR r/m16, 1
+// SAR r/m8, CL
+// SAR r/m16, CL
+static ExecuteInstructionStatus ExecuteGroup2Sar(
+    const InstructionContext* ctx, Operand* op, uint8_t count) {
+  // Return early if count is 0, so as to not affect flags.
+  if (count == 0) {
+    return kExecuteSuccess;
+  }
+  int32_t value = FromSignedOperand(op);
+  int32_t result = value >> count;
+  WriteOperand(ctx, op, result);
+  bool last_lsb = ((value >> (count - 1)) & 1) != 0;
+  SetFlag(ctx->cpu, kCF, last_lsb);
+  if (count == 1) {
+    SetFlag(ctx->cpu, kOF, false);
+  }
+  SetCommonFlagsAfterInstruction(ctx, result);
+  return kExecuteSuccess;
+}
+
+// ROL r/m8, 1
+// ROL r/m16, 1
+// ROL r/m8, CL
+// ROL r/m16, CL
+static ExecuteInstructionStatus ExecuteGroup2Rol(
+    const InstructionContext* ctx, Operand* op, uint8_t count) {
+  // Return early if count is 0, so as to not affect flags.
+  if (count == 0) {
+    return kExecuteSuccess;
+  }
+  // The 8086 computes the modulus of the count after the zero check, which is
+  // different than the 80286 and later processors.
+  uint8_t effective_count = count % kNumBits[ctx->metadata->width];
+  uint32_t value = FromOperand(op);
+  uint32_t result =
+      (value << effective_count) |
+      (value >> (kNumBits[ctx->metadata->width] - effective_count));
+  WriteOperand(ctx, op, result);
+  bool last_msb = (result & 1) != 0;
+  SetFlag(ctx->cpu, kCF, last_msb);
+  if (count == 1) {
+    bool current_msb = ((result & kSignBit[ctx->metadata->width]) != 0);
+    SetFlag(ctx->cpu, kOF, last_msb != current_msb);
+  }
+  return kExecuteSuccess;
+}
+
+// ROR r/m8, 1
+// ROR r/m16, 1
+// ROR r/m8, CL
+// ROR r/m16, CL
+static ExecuteInstructionStatus ExecuteGroup2Ror(
+    const InstructionContext* ctx, Operand* op, uint8_t count) {
+  // Return early if count is 0, so as to not affect flags.
+  if (count == 0) {
+    return kExecuteSuccess;
+  }
+  // The 8086 computes the modulus of the count after the zero check, which is
+  // different than the 80286 and later processors.
+  uint8_t effective_count = count % kNumBits[ctx->metadata->width];
+  uint32_t value = FromOperand(op);
+  uint32_t result =
+      (value >> effective_count) |
+      (value << (kNumBits[ctx->metadata->width] - effective_count));
+  WriteOperand(ctx, op, result);
+  bool last_lsb = (result & kSignBit[ctx->metadata->width]) != 0;
+  SetFlag(ctx->cpu, kCF, last_lsb);
+  if (count == 1) {
+    bool original_msb = ((value & kSignBit[ctx->metadata->width]) != 0);
+    SetFlag(ctx->cpu, kOF, last_lsb != original_msb);
+  }
+  return kExecuteSuccess;
+}
+
+// RCL r/m8, 1
+// RCL r/m16, 1
+// RCL r/m8, CL
+// RCL r/m16, CL
+static ExecuteInstructionStatus ExecuteGroup2Rcl(
+    const InstructionContext* ctx, Operand* op, uint8_t count) {
+  uint8_t effective_count = count % (kNumBits[ctx->metadata->width] + 1);
+  if (effective_count == 0) {
+    return kExecuteSuccess;
+  }
+  uint32_t cf_value = GetFlag(ctx->cpu, kCF) ? (1 << (effective_count - 1)) : 0;
+  uint32_t value = FromOperand(op);
+  uint32_t result =
+      (value << effective_count) | cf_value |
+      (value >> (kNumBits[ctx->metadata->width] - (effective_count - 1)));
+  WriteOperand(ctx, op, result);
+  bool last_msb =
+      ((value << (effective_count - 1)) & kSignBit[ctx->metadata->width]) != 0;
+  SetFlag(ctx->cpu, kCF, last_msb);
+  if (count == 1) {
+    bool current_msb = ((result & kSignBit[ctx->metadata->width]) != 0);
+    SetFlag(ctx->cpu, kOF, last_msb != current_msb);
+  }
+  return kExecuteSuccess;
+}
+
+// RCR r/m8, 1
+// RCR r/m16, 1
+// RCR r/m8, CL
+// RCR r/m16, CL
+static ExecuteInstructionStatus ExecuteGroup2Rcr(
+    const InstructionContext* ctx, Operand* op, uint8_t count) {
+  uint8_t effective_count = count % (kNumBits[ctx->metadata->width] + 1);
+  if (effective_count == 0) {
+    return kExecuteSuccess;
+  }
+  uint32_t cf_value =
+      GetFlag(ctx->cpu, kCF)
+          ? (kSignBit[ctx->metadata->width] >> (effective_count - 1))
+          : 0;
+  uint32_t value = FromOperand(op);
+  uint32_t result =
+      (value >> effective_count) | cf_value |
+      (value << (kNumBits[ctx->metadata->width] - (effective_count - 1)));
+  WriteOperand(ctx, op, result);
+  bool last_lsb = ((value >> (effective_count - 1)) & 1) != 0;
+  SetFlag(ctx->cpu, kCF, last_lsb);
+  if (count == 1) {
+    bool original_msb = ((value & kSignBit[ctx->metadata->width]) != 0);
+    bool current_msb = ((result & kSignBit[ctx->metadata->width]) != 0);
+    SetFlag(ctx->cpu, kOF, current_msb != original_msb);
+  }
+  return kExecuteSuccess;
+}
+
+static const Group2ExecuteInstructionFn kGroup2ExecuteInstructionFns[] = {
+    ExecuteGroup2Rol,  // 0 - ROL
+    ExecuteGroup2Ror,  // 1 - ROR
+    ExecuteGroup2Rcl,  // 2 - RCL
+    ExecuteGroup2Rcr,  // 3 - RCR
+    ExecuteGroup2Shl,  // 4 - SHL
+    ExecuteGroup2Shr,  // 5 - SHR
+    ExecuteGroup2Shl,  // 6 - SAL (same as SHL)
+    ExecuteGroup2Sar,  // 7 - SAR
+};
+
+// Group 2 shift / rotate by 1.
+static ExecuteInstructionStatus ExecuteGroup2ShiftOrRotateBy1Instruction(
+    const InstructionContext* ctx) {
+  const Group2ExecuteInstructionFn fn =
+      kGroup2ExecuteInstructionFns[ctx->instruction->mod_rm.reg];
+  Operand op = ReadRegisterOrMemoryOperand(ctx);
+  return fn(ctx, &op, 1);
+}
+
+// Group 2 shift / rotate by CL.
+static ExecuteInstructionStatus ExecuteGroup2ShiftOrRotateByCLInstruction(
+    const InstructionContext* ctx) {
+  const Group2ExecuteInstructionFn fn =
+      kGroup2ExecuteInstructionFns[ctx->instruction->mod_rm.reg];
+  Operand op = ReadRegisterOrMemoryOperand(ctx);
+  return fn(ctx, &op, ctx->cpu->registers[kCX] & 0xFF);
+}
+
+// ============================================================================
 // Group 3 - TEST, NOT, NEG, MUL, IMUL, DIV, IDIV
 // ============================================================================
 
@@ -1829,7 +2048,7 @@ static ExecuteInstructionStatus ExecuteDiv(
           ctx->cpu, &kMulDivResultHighHalfAddress[width]);
   uint32_t dividend =
       FromOperand(&dest) | (FromOperandValue(&dest_high_half)
-                           << kMulDivResultHighHalfShiftWidth[width]);
+                            << kMulDivResultHighHalfShiftWidth[width]);
   uint32_t quotient = dividend / divisor;
   if (quotient > kMaxValue[ctx->metadata->width]) {
     // TODO: Handle division overflow error
@@ -1856,7 +2075,7 @@ static ExecuteInstructionStatus ExecuteIdiv(
           ctx->cpu, &kMulDivResultHighHalfAddress[width]);
   int32_t dividend =
       FromOperand(&dest) | (FromSignedOperandValue(&dest_high_half)
-                           << kMulDivResultHighHalfShiftWidth[width]);
+                            << kMulDivResultHighHalfShiftWidth[width]);
   int32_t quotient = dividend / divisor;
   if (quotient > kMaxSignedValue[ctx->metadata->width] ||
       quotient < kMinSignedValue[ctx->metadata->width]) {
@@ -3015,13 +3234,29 @@ static const OpcodeMetadata opcodes[] = {
     // IRET
     {.opcode = 0xCF, .has_modrm = false, .immediate_size = 0},
     // ROL/ROR/RCL/RCR/SHL/SHR/SAR r/m8, 1 (Group 2)
-    {.opcode = 0xD0, .has_modrm = true, .immediate_size = 0, .width = kByte},
+    {.opcode = 0xD0,
+     .has_modrm = true,
+     .immediate_size = 0,
+     .width = kByte,
+     .handler = ExecuteGroup2ShiftOrRotateBy1Instruction},
     // ROL/ROR/RCL/RCR/SHL/SHR/SAR r/m16, 1 (Group 2)
-    {.opcode = 0xD1, .has_modrm = true, .immediate_size = 0, .width = kWord},
+    {.opcode = 0xD1,
+     .has_modrm = true,
+     .immediate_size = 0,
+     .width = kWord,
+     .handler = ExecuteGroup2ShiftOrRotateBy1Instruction},
     // ROL/ROR/RCL/RCR/SHL/SHR/SAR r/m8, CL (Group 2)
-    {.opcode = 0xD2, .has_modrm = true, .immediate_size = 0, .width = kByte},
+    {.opcode = 0xD2,
+     .has_modrm = true,
+     .immediate_size = 0,
+     .width = kByte,
+     .handler = ExecuteGroup2ShiftOrRotateByCLInstruction},
     // ROL/ROR/RCL/RCR/SHL/SHR/SAR r/m16, CL (Group 2)
-    {.opcode = 0xD3, .has_modrm = true, .immediate_size = 0, .width = kWord},
+    {.opcode = 0xD3,
+     .has_modrm = true,
+     .immediate_size = 0,
+     .width = kWord,
+     .handler = ExecuteGroup2ShiftOrRotateByCLInstruction},
     // AAM
     {.opcode = 0xD4, .has_modrm = false, .immediate_size = 1},
     // AAD
