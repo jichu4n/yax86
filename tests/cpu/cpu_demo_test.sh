9bb5f0519145188a1f036c786e4e460cca5a8993
Chuan Ji <chuan@jichu4n.com>
2025-07-08 00:14:43 -0700

    Move CPU demo program to tools/cpu_demo.

--- tests/cpu/cpu_demo_test.sh
+++ tests/cpu/cpu_demo_test.sh
@@ -3,14 +3,15 @@
 root_dir="$(dirname "$0")/../.."
 cd "$root_dir" || exit 1
 
-cpu_demo=./build/demos/cpu/cpu_demo
+cpu_demo=./build/tools/cpu_demo/cpu_demo
+asm_dir=./tools/cpu_demo
 
 set -xe
 
-echo -e '12\n25\n' | build/demos/cpu/cpu_demo demos/cpu/horoscope.asm | grep Capricorn
-echo -e '12\n15\n' | build/demos/cpu/cpu_demo demos/cpu/horoscope.asm | grep Sagittarius
-echo -e '05\n01\n' | build/demos/cpu/cpu_demo demos/cpu/horoscope.asm | grep Taurus
+echo -e '12\n25\n' | $cpu_demo $asm_dir/horoscope.asm | grep Capricorn
+echo -e '12\n15\n' | $cpu_demo $asm_dir/horoscope.asm | grep Sagittarius
+echo -e '05\n01\n' | $cpu_demo $asm_dir/horoscope.asm | grep Taurus
 
 for i in {1..12}; do
-  echo -e "2025\n${i}\n" | build/demos/cpu/cpu_demo demos/cpu/cal.asm
+  echo -e "2025\n${i}\n" | build/tools/cpu_demo/cpu_demo $asm_dir/cal.asm
 done
